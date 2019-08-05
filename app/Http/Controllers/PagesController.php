b589c870565caff5cf039278f82e84f08c656044
christopher Njoku <ccn9@njit.edu>
2019-08-04 23:44:46 -0400

    first commit project

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -18,7 +18,7 @@ class PagesController extends Controller
     function store(Request $request) {
         $name = $request->name;
       
-       return redirect()->route('thanks', ['name' => $name]);
+       return redirect()->route('thanks',['name' => $name]);
     }
     function thanks($name, Request $request) {
 
